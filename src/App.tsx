765c3508470b0e6e37475426620ffc0b55d38bba
Victor Porton <porton.victor@gmail.com>
2022-12-19 04:50:49 +0200

    text

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,9 @@ function App() {
         <h1>World Science DAO accepts donations</h1>
         <p>To donate send xDai or any ERC-20 token to <code className="cryptoAddress">{donationsAddress}</code>
         on <span className="cryptoAddress">Gnosis</span> (formerly called <span className="cryptoAddress">Dai</span>) chain.</p>
-        <p><strong>Funds send to this address on any other chain, including main Ethereum chain will be irreversibly lost!</strong></p>
-        <p>Or <a href="#" onClick={initCardAppDonation}>donate by credit card</a>.</p>
-        <p>Credit card donations are tax-deductible, because they are considered as donations
+        <p><strong className="danger">Funds send to this address on any other chain, including main Ethereum chain will be irreversibly lost!</strong></p>
+        <p>Or <a href="#" onClick={initCardAppDonation}>donate by <strong>credit card</strong> or SEPA, etc.</a></p>
+        <p>Credit card, SEPA, etc. donations are tax-deductible, because they are considered as donations
           to 501(c)3 Victor Porton's Foundation that are immediately and fully transfered to World Science DAO.</p>
         <p><a href="https://science-dao.vporton.name" target="_top">Return to World Science DAO.</a></p>
         <p><a href="https://github.com/vporton/science-dao-donate" target='_blank' rel="noreferrer">
