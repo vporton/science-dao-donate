786b7a627368c9d5b230671b3f190d034b3b3c14
Victor Porton <porton.victor@gmail.com>
2022-12-20 10:55:51 +0200

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,84 @@
-import { useEffect } from 'react';
-import { donationsAddress, donationsNetwork, rampApiKey } from './config';
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Button, Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, useAccount, useBalance, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+import { useEffect, useState } from 'react';
+import { donationsAddress, donationsNetwork, rampApiKey, walletConnectProjectId } from './config';
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
 import './App.css';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { formatEther } from "ethers/lib/utils.js";
+import { BigNumber } from "ethers";
+
+const chains = [arbitrum, mainnet, polygon]; // FIXME
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: walletConnectProjectId }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+function DonateCryptoButton() {
+  const [open, setOpen] = useState(false);
+  const { address } = useAccount();
+  const { data: balanceData } = useBalance({ address });
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  function balanceMinusGas() {
+    return balanceData?.value.sub(21000); // for simple transfers, we assume contract has no hook here
+  }
+  function balanceMinusGasFormatted() {
+    const balance = balanceMinusGas();
+    return balance !== undefined ? formatEther(balance as BigNumber) : null;
+  }
+  return (
+    <span>
+      <Button
+        onClick={handleClickOpen}
+        variant='contained'
+        disabled={address === undefined}
+      >
+        donate
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Donate</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Amount ($)"
+            type="number"
+            fullWidth
+            variant="standard"
+            defaultValue={balanceMinusGasFormatted()}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Donate</Button>
+        </DialogActions>
+      </Dialog>
+    </span>
+  );
+
+}
 
 async function initCardAppDonation() {
   const logo = `${document.location.protocol}//${document.location.host}${document.location.pathname}logo.svg`;
@@ -14,6 +91,29 @@ async function initCardAppDonation() {
   }).show();
 }
 
+function AppMainPart() {
+  return (
+    <>
+      <p>Connected wallet: <span style={{display: 'inline-block', verticalAlign: 'middle'}}><Web3Button /></span></p>
+      <h1>World Science DAO accepts donations</h1>
+      <p><strong className="danger">WRONG CHAIN</strong></p>
+      <p><strong>Donate by crypto or credit card.</strong></p>
+      <p>To <span style={{display: 'inline-block'}}><DonateCryptoButton/></span> send xDai or any ERC-20 token to <code className="cryptoAddress">{donationsAddress}</code> {' '}
+      on <span className="cryptoAddress">Gnosis</span> (formerly called <span className="cryptoAddress">Dai</span>) chain.</p>
+      <p><strong className="danger">Funds sent to this address on any other chain, including main Ethereum chain, will be irreversibly lost!</strong></p>
+      <p>You can first <a href="https://coinmarketcap.com/currencies/wxdai/markets/" target="markets">purchase wxDai</a> {' '}
+        and then <a href="https://app.openocean.finance/CLASSIC#/XDAI/WXDAI/XDAI" target="markets">swap it for xDai</a>.</p>
+      <p>Or <button onClick={initCardAppDonation}>donate by <strong>credit card</strong> or SEPA, etc.</button></p>
+      <p>Credit card, SEPA, etc. donations are tax-deductible, because they are considered as donations
+        to 501(c)3 Victor Porton's Foundation that are immediately and fully transfered to World Science DAO.</p>
+      <p><a href="https://science-dao.vporton.name" target="_top">Return to World Science DAO.</a></p>
+      <p><a href="https://github.com/vporton/science-dao-donate" target='_blank' rel="noreferrer">
+        <img src="github-mark.svg" width="16" height="16" alt="GitHub"/></a>
+      </p>
+    </>
+  );
+}
+
 function App() {
   useEffect(() => {
     document.title = 'Donations - World Science DAO';
@@ -21,20 +121,13 @@ function App() {
   return (
     <div className="App">
       <div className="mainWidget">
-        <h1>World Science DAO accepts donations</h1>
-        <p><strong>Donate by crypto or credit card.</strong></p>
-        <p>To donate send xDai or any ERC-20 token to <code className="cryptoAddress">{donationsAddress}</code> {' '}
-        on <span className="cryptoAddress">Gnosis</span> (formerly called <span className="cryptoAddress">Dai</span>) chain.</p>
-        <p><strong className="danger">Funds sent to this address on any other chain, including main Ethereum chain, will be irreversibly lost!</strong></p>
-        <p>You can first <a href="https://coinmarketcap.com/currencies/wxdai/markets/" target="markets">purchase wxDai</a> {' '}
-          and then <a href="https://app.openocean.finance/CLASSIC#/XDAI/WXDAI/XDAI" target="markets">swap it for xDai</a>.</p>
-        <p>Or <button onClick={initCardAppDonation}>donate by <strong>credit card</strong> or SEPA, etc.</button></p>
-        <p>Credit card, SEPA, etc. donations are tax-deductible, because they are considered as donations
-          to 501(c)3 Victor Porton's Foundation that are immediately and fully transfered to World Science DAO.</p>
-        <p><a href="https://science-dao.vporton.name" target="_top">Return to World Science DAO.</a></p>
-        <p><a href="https://github.com/vporton/science-dao-donate" target='_blank' rel="noreferrer">
-          <img src="github-mark.svg" width="16" height="16" alt="GitHub"/></a>
-        </p>
+        <WagmiConfig client={wagmiClient}>
+          <AppMainPart/>
+        </WagmiConfig>
+        <Web3Modal
+          projectId={walletConnectProjectId}
+          ethereumClient={ethereumClient}
+        />
       </div>
     </div>
   );
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const donationsAddress = "0x61d5A4542Fc28703B4267Cad28020c5E193A628f";
 export const donationsNetwork = "XDAI";
-export const rampApiKey = 'gd9nmr8grvvecoxerfstt3mapj35mbgcyb7krqcm';
+export const rampApiKey = "gd9nmr8grvvecoxerfstt3mapj35mbgcyb7krqcm";
+export const walletConnectProjectId = "1b73cc17532df865d3f9377b3463fc42";
